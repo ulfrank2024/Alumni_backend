520f7a767232a3272c4030b14054bad02b9e7462
ulfrank2024 <2724548@collegelacite.ca>
2025-05-23 23:45:32 -0400

    commit 6

--- server.js
+++ server.js
@@ -7,19 +7,28 @@ dotenv.config();
 
 const app = express();
 
+const allowedOrigins = ["https://ulfrank2024.github.io"];
+
 app.use(
     cors({
-        origin: "https://ulfrank2024.github.io",
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        methods: ["GET", "POST", "OPTIONS"],
+        allowedHeaders: ["Content-Type"],
     })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.set("view engine", "ejs");
-
 app.use("/api/form", formRoutes);
 app.get("/admin", (req, res) => {
-    // res.render("admin"); // Vue à créer
+    // res.render("admin");
 });
 
 const PORT = process.env.PORT || 5000;
