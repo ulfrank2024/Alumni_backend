4ba144537c4009b1fd2b34fba9875027e715547d
ulfrank2024 <2724548@collegelacite.ca>
2025-05-24 13:34:22 -0400

    commit 13

--- controllers/formController.js
+++ controllers/formController.js
@@ -16,6 +16,14 @@ exports.submitForm = async (req, res) => {
             skillsUsefulness,
             recommend,
             testimonial,
+            communication,
+            activite,
+            souvenir,
+            conseil,
+            agriculture,
+            tutore,
+            ecole,
+            centre,
             willingToTeach,
             teachingFields,
             partnershipSuggestions,
@@ -35,6 +43,7 @@ exports.submitForm = async (req, res) => {
             workStudy,
             entrepreneurshipCulture,
         } = req.body;
+        
 
         // 🔍 Vérifier si l'email a déjà été utilisé
         const checkEmail = await db.query(
@@ -53,14 +62,20 @@ exports.submitForm = async (req, res) => {
             `INSERT INTO responses (
               name, email, program, field, promotion_year, residence_country, current_job, current_company,
               teaching_quality, skills_usefulness, recommend, testimonial,
+              communication, activite, souvenir, conseil, agriculture, tutore,  ecole, centre,
               willing_to_teach, teaching_fields, partnership_suggestions, willing_to_support_partnership,
               abroad, certification_issue, certification_suggestion,
               award_admin, admin_award_details, admin_award_type,
               award_alumni, alumni_award_details, alumni_award_type,
               strengths, improvements, vision_uneep, work_study, entrepreneurship_culture
-            ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,
-                      $13,$14,$15,$16,$17,$18,$19,$20,$21,$22,
-                      $23,$24,$25,$26,$27,$28,$29,$30)`,
+            ) VALUES (
+              $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,
+              $13,$14,$15,$16,$17,$18,$19,
+              $20,$21,$22,$23,$24,$25,$26,
+              $27,$28,$29,
+              $30,$31,$32,
+              $33,$34,$35,$36,$37,$38,$39
+            )`,
             [
                 name,
                 email,
@@ -74,6 +89,14 @@ exports.submitForm = async (req, res) => {
                 skillsUsefulness,
                 recommend,
                 testimonial,
+                communication,
+                activite,
+                souvenir,
+                conseil,
+                agriculture,
+                tutore,
+                ecole,
+                centre,
                 willingToTeach,
                 teachingFields,
                 partnershipSuggestions,
@@ -94,7 +117,7 @@ exports.submitForm = async (req, res) => {
                 entrepreneurshipCulture,
             ]
         );
-
+        
         await sendConfirmationEmail(email, name);
 
         res.status(200).json({ message: "Formulaire soumis avec succès." });
